72698890b6df23bbcbcbd33317ac95c87dae2f11
MarcelinoFreyre <marceeunion@gmail.com>
2022-04-25 23:52:42 -0300

    commit 6

---
+++ js/date.js
@@ -0,0 +1,11 @@
+try {
+    let options {
+        weekday: "long",
+        day : "numeric",
+        month: "long",
+        year: "numeric",
+    };
+    document.getElementById("currentday").textContent = new Date().toLocaleDateString("en-US", options);
+} catch(e) {
+    alert("Error with code or your browser does not support Locale");
+}
--- js/index.js
+++ js/index.js
@@ -1,2 +1,2 @@
-alert(document.lastModified);
-let oLastModif = new Date(document.lastModified);
+// alert(document.lastModified);
+// let oLastModif = new Date(document.lastModified);
