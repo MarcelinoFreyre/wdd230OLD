9484360ab7afb28da60cafb08e7a71e8fb7b0d18
MarcelinoFreyre <marceeunion@gmail.com>
2022-04-30 23:54:19 -0300

    commmit 12

--- js/index.js
+++ js/index.js
@@ -1,7 +1,2 @@
-/* function lastModified() {
-    var x = document.lastModified;
-    console.log(x);
-    document.getElementById("sudo").innerHTML = x;
-} */
 let text = document.lastModified;
 document.getElementById("demo").innerHTML = text;
